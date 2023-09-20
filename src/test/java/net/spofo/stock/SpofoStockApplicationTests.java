75a37df2936a03a5462ea35dbd22bf68d576c85d
psg <seunggyu.dev@gmail.com>
2023-09-20 15:08:19 +0900

    :sparkles: [Feat] : 프로젝트 생성
    초기 프로젝트 설정 완료.

---
+++ src/test/java/net/spofo/stock/SpofoStockApplicationTests.java
@@ -0,0 +1,13 @@
+package net.spofo.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpofoStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
