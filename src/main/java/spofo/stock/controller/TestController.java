3a9b6549639bbc5721c3df2d9d0789369db0da39
psg <seunggyu.dev@gmail.com>
2023-09-22 15:14:27 +0900

    :sparkles: [Feat] : 서버 간 통신 설정

--- src/main/java/spofo/stock/controller/TestController.java
+++ src/main/java/spofo/stock/controller/TestController.java
@@ -2,6 +2,7 @@ package spofo.stock.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
 @RestController
 public class TestController {
@@ -14,5 +15,29 @@ public class TestController {
     @GetMapping("/test/callStock")
     public String testCallStock() {
         return "/test/callStock - 주식 서버 호출 성공했습니다.";
+    }
+
+    @GetMapping("/test/callPortfolio")
+    public String testCallPortfolio() {
+        RestClient restClient = RestClient.create();
+
+        String resultByPortfolio = restClient.get()
+                .uri("http://portfolio.spofo.net/test")
+                .retrieve()
+                .body(String.class);
+
+        return "주식 서버가 포트폴리오 서버를 호출했습니다. : " + resultByPortfolio;
+    }
+
+    @GetMapping("/test/callAuth")
+    public String testCallAuth() {
+        RestClient restClient = RestClient.create();
+
+        String resultByAuth = restClient.get()
+                .uri("http://auth.spofo.net/test")
+                .retrieve()
+                .body(String.class);
+
+        return "주식 서버가 인증 서버를 호출했습니다. : " + resultByAuth;
     }
 }
