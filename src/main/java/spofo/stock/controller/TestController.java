13ce0f2b993c460173563cd740b96e81e0fbfa74
psg <seunggyu.dev@gmail.com>
2023-09-22 14:48:14 +0900

    :sparkles: [Feat] : CORS 설정 구현

--- src/main/java/spofo/stock/controller/TestController.java
+++ src/main/java/spofo/stock/controller/TestController.java
@@ -1,5 +1,6 @@
 package spofo.stock.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,11 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/test")
+    @CrossOrigin("*")
     public String testResult() {
         return "/test - 주식 서버입니다.";
     }
 
     @GetMapping("/test/callStock")
+    @CrossOrigin("*")
     public String testCallStock() {
         return "/test/callStock - 주식 서버 호출 성공했습니다.";
     }
