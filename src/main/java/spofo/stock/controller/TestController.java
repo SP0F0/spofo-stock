057e5d6636c892ee79ebc834a939a903ce22352a
psg <seunggyu.dev@gmail.com>
2023-09-22 15:03:55 +0900

    :sparkles: [Feat] : 서버 간 통신 테스트

--- src/main/java/spofo/stock/controller/TestController.java
+++ src/main/java/spofo/stock/controller/TestController.java
@@ -3,19 +3,31 @@ package spofo.stock.controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
 @RestController
 public class TestController {
 
     @GetMapping("/test")
-    @CrossOrigin("*")
     public String testResult() {
         return "/test - 주식 서버입니다.";
     }
 
     @GetMapping("/test/callStock")
-    @CrossOrigin("*")
     public String testCallStock() {
         return "/test/callStock - 주식 서버 호출 성공했습니다.";
+    }
+
+    @GetMapping("/test/authTest")
+    public String testCallAuth() {
+
+        RestClient restClient = RestClient.create();
+
+        String resultByAuth = restClient.get()
+                .uri("http://portfolio.spofo.net:8080/test")
+                .retrieve()
+                .body(String.class);
+
+        return "주식 서버로부터 포트폴리오 서버를 호출한 메시지 입니다. " + resultByAuth;
     }
 }
