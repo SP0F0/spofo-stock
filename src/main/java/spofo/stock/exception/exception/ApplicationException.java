5f4201e268a1a7778abc1f34d388e5d80361bf67
psg <seunggyu.dev@gmail.com>
2023-10-01 17:25:44 +0900

    :sparkles: [Feat] : 예외 처리 핸들러 구현
    추후에 에러 코드 다양하게 수정 예정

---
+++ src/main/java/spofo/stock/exception/exception/ApplicationException.java
@@ -0,0 +1,29 @@
+package spofo.stock.exception.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public class ApplicationException extends RuntimeException {
+
+    private ErrorCode errorCode;
+    private HttpStatus httpStatus;
+    private String message;
+
+    public ApplicationException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.httpStatus = errorCode.getHttpStatus();
+        this.message = null;
+    }
+
+    @Override
+    public String getMessage() {
+        if (message == null) {
+            return errorCode.getMessage();
+        } else {
+            return String.format("%s : %s", errorCode.getMessage(), message);
+        }
+    }
+}
