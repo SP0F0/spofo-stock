0700479cfce95fd8218dcaf7b3156b517b68195e
psg <seunggyu.dev@gmail.com>
2023-10-17 14:16:35 +0900

    🔨 [Refactor] : 사용하지 않는 생성자 삭제

--- src/main/java/spofo/stock/data/request/kis/KisRequestDto.java
+++ src/main/java/spofo/stock/data/request/kis/KisRequestDto.java
@@ -14,11 +14,4 @@ public class KisRequestDto {
     private String rt_cd;
     private String msg_cd;
     private String msg1;
-
-    public KisRequestDto(Output output, String rt_cd, String msg_cd, String msg1) {
-        this.output = Optional.of(output).orElseGet(() -> new Output());
-        this.rt_cd = rt_cd;
-        this.msg_cd = msg_cd;
-        this.msg1 = msg1;
-    }
 }
