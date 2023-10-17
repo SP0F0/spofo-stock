82cec7256083a8032bcad4c3396a477bcebd788d
psg <seunggyu.dev@gmail.com>
2023-10-17 16:10:29 +0900

    ✨ [Feat] : 외부 API 호출 시 필요한 토큰이 비정상이면 반환해야 할 HTTP 상태코드, 메시지 구현

--- src/main/java/spofo/stock/exception/ErrorCode.java
+++ src/main/java/spofo/stock/exception/ErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러 발생"),
+    INVALID_KIS_KEY(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰")
     ;
     private final HttpStatus httpStatus;
     private final String message;
