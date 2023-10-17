7508113f85b5c418f588e2356090091c3d3ced51
psg <seunggyu.dev@gmail.com>
2023-10-17 17:01:24 +0900

    🔨 [Refactor] : 사용하지 않는 어노테이션 삭제

--- src/main/java/spofo/stock/exception/GlobalControllerAdvice.java
+++ src/main/java/spofo/stock/exception/GlobalControllerAdvice.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-@RequiredArgsConstructor
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(RuntimeException.class)
