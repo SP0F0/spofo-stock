93bd480833bcbd3e12523289fd6f973d0be7a062
psg <seunggyu.dev@gmail.com>
2023-10-17 16:37:34 +0900

    ✨ [Feat] : 유효하지 않은 종목 코드를 요청했을 때 반환하는 상태 코드, 메시지 인스턴스 추가.

--- src/main/java/spofo/stock/exception/ErrorCode.java
+++ src/main/java/spofo/stock/exception/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러 발생"),
-    INVALID_KIS_KEY(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰")
+    INVALID_KIS_KEY(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰"),
+    INVALID_STOCK_CODE(HttpStatus.BAD_REQUEST, "유효하지 않은 종목 코드")
     ;
     private final HttpStatus httpStatus;
     private final String message;
