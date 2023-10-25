998cf4ba8bf1c04852a3c155c477c7f68103678b
psg <seunggyu.dev@gmail.com>
2023-10-25 15:59:02 +0900

    ✨️ [Feat] : Stock 객체로 현재가 Dto 만드는 메서드

--- src/main/java/spofo/stock/data/StockCurrentPriceResponseDto.java
+++ src/main/java/spofo/stock/data/StockCurrentPriceResponseDto.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import spofo.stock.data.request.kis.Output;
+import spofo.stock.schedule.entity.Stock;
 
 @Getter
 @NoArgsConstructor
@@ -18,15 +19,17 @@ public class StockCurrentPriceResponseDto {
 
     private String name;
     private String price;
+    private String imageUrl;
     private String market;
     private String sector;
 
     @Builder
-    private StockCurrentPriceResponseDto(String code, String name, String price, String market,
+    private StockCurrentPriceResponseDto(String code, String name, String price, String market, String imageUrl,
             String sector) {
         this.code = code;
         this.name = name;
         this.price = price;
+        this.imageUrl = imageUrl;
         this.market = market;
         this.sector = sector;
     }
@@ -38,6 +41,17 @@ public class StockCurrentPriceResponseDto {
                 .market(output.getRprs_mrkt_kor_name())
                 .sector(output.getBstp_kor_isnm())
                 .code(output.getStck_shrn_iscd())
+                .build();
+    }
+
+    public static StockCurrentPriceResponseDto of(Output output, Stock stock) {
+        return StockCurrentPriceResponseDto.builder()
+                .code(output.getStck_shrn_iscd())
+                .name(stock.getName())
+                .price(output.getStck_prpr())
+                .imageUrl(stock.getImageUrl())
+                .market(output.getRprs_mrkt_kor_name())
+                .sector(output.getBstp_kor_isnm())
                 .build();
     }
 }
