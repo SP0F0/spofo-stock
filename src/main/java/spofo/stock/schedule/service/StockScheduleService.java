99738f7996f5e8fb0a1c375f34160e4d165ecf90
psg <seunggyu.dev@gmail.com>
2023-10-25 09:43:13 +0900

    ✨ [Feat] : 주식 정보 스케줄링 서비스 클래스 생성

---
+++ src/main/java/spofo/stock/schedule/service/StockScheduleService.java
@@ -0,0 +1,22 @@
+package spofo.stock.schedule.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+import spofo.stock.schedule.repository.StockScheduleRedisRepository;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class StockScheduleService {
+
+    private final RestClient restClient;
+    private final StockScheduleRedisRepository stockScheduleRedisRepository;
+    private static final String publicDataUrl = "https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService";
+    private static final String publicDataPath = "/getStockPriceInfo";
+
+    @Value("${public.data.decodingKey}")
+    private String decodingKey;
+}
