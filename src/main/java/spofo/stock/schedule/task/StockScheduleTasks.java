df7b61cef1da96410291d421425df68d7c5e48c0
psg <seunggyu.dev@gmail.com>
2023-10-25 11:14:34 +0900

    ⚡️ [Feat] : 최근 주식 개장 날짜 구하는 메서드 구현

--- src/main/java/spofo/stock/schedule/task/StockScheduleTasks.java
+++ src/main/java/spofo/stock/schedule/task/StockScheduleTasks.java
@@ -3,9 +3,12 @@ package spofo.stock.schedule.task;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
-import spofo.stock.schedule.repository.StockScheduleRedisRepository;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+import spofo.stock.data.request.publicdata.PublicDataRequestDto;
 
 @Slf4j
 @Service
@@ -13,12 +16,41 @@ import spofo.stock.schedule.repository.StockScheduleRedisRepository;
 public class StockScheduleTasks {
 
     private final RestClient restClient;
-    private final StockScheduleRedisRepository stockScheduleRedisRepository;
     private static final String publicDataUrl = "https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService";
     private static final String publicDataPath = "/getStockPriceInfo";
 
     @Value("${public.data.decodingKey}")
     private String decodingKey;
 
+    public void saveStocks() {
 
+        String recentTradingDate = getRecentTradeDate();
+
+    }
+
+    private String getRecentTradeDate() {
+
+        UriComponents uri = UriComponentsBuilder
+                .fromUriString(publicDataUrl)
+                .path(publicDataPath)
+                .queryParam("numOfRows", "{num}")
+                .queryParam("serviceKey", "{key}")
+                .queryParam("resultType", "{type}")
+                .encode()
+                .buildAndExpand(1, decodingKey, "json");
+
+        PublicDataRequestDto response = restClient.get()
+                .uri(uri.toUri())
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .body(PublicDataRequestDto.class);
+
+        String recentTradingDate = response.getResponse().getBody().getItems()
+                .getItem()
+                .get(0)
+                .getBasDt();
+
+        log.info("recentTradingDate = {}", recentTradingDate);
+        return recentTradingDate;
+    }
 }
